1853379c0358c1b365963b09afef6e177f702479
awang923 <awang144@ucr.edu>
2020-11-16 18:05:06 -0800

    rm incorrect files

--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-#include "rectangle.hpp"
-#include "test_suites.hpp"
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
